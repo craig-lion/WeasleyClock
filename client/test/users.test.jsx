dabb60a3b59f896bab4469c66626ab4f2465931f
Lion <craig.m.lindahl@gmail.com>
2020-06-30 09:27:07 -0700

    Test environment set up, initial tests verified and working

--- client/test/users.test.jsx
+++ client/test/users.test.jsx
@@ -1,44 +1,39 @@
-import 'regenerator-runtime/runtime';
+import regeneratorRuntime from 'regenerator-runtime';
+import React from 'react';
 import {
   describe, expect, it, beforeEach, afterEach, jest,
 } from '@jest/globals';
-import { render, fireEvent } from '@testing-library/react';
-import React from 'react';
+import {
+  render, fireEvent, waitFor, screen,
+} from '@testing-library/react';
 import axios from 'axios';
 import Login from '../src/components/Login';
 
 const domTestingLib = require('@testing-library/dom');
 
 const { queryHelpers } = domTestingLib;
+const getById = queryHelpers.queryByAttribute.bind(null, 'id');
 
+const mockSetState = jest.fn();
 jest.mock('axios');
 
 describe('users', () => {
   let wrapper;
-
   describe('authentication', () => {
-    const setState = jest.fn();
-    const useStateSpy = jest.spyOn(React, 'useState');
-    useStateSpy.mockImplementation((init) => [init, setState]);
-    const postPromise = Promise.resolve({ data: true });
-    axios.post.mockImplementation(() => postPromise);
-    const setLoginUserName = jest.fn();
-    const setIsLoggedIn = jest.fn();
-    // const { asFragment } = render(<Login
-    //   setLoginUserName={setLoginUserName}
-    //   setIsLoggedIn={setIsLoggedIn}
-    // />);
-
-    // it('FragmentTest', () => {
-
-    //   expect(asFragment()).toMatchSnapshot();
-    // });
+    let setLoginUserName;
+    let setIsLoggedIn;
+    let postPromise;
 
     beforeEach(() => {
-      // wrapper = Enzyme.mount((<Login
-      //   setLoginUserName={setLoginUserName}
-      //   setIsLoggedIn={setIsLoggedIn}
-      // />));
+      jest.mock('react', () => ({
+        useState: (initial) => [initial, mockSetState],
+      }));
+
+      postPromise = Promise.resolve({ data: true });
+      axios.post.mockImplementation(() => postPromise);
+      setLoginUserName = jest.fn();
+      setIsLoggedIn = jest.fn();
+
       wrapper = render(<Login
         setLoginUserName={setLoginUserName}
         setIsLoggedIn={setIsLoggedIn}
@@ -49,45 +44,31 @@ describe('users', () => {
       jest.clearAllMocks();
     });
 
-    it('can create new user', async () => {
-      const getById = queryHelpers.queryByAttribute.bind(null, 'id');
-      fireEvent.click(getById(wrapper.container, 'newUser'));
-      expect(axios.post).toHaveBeenCalledWith('/api/addUser', { userName: expect.any(String), password: expect.any(String) });
-      await postPromise;
-      expect(setLoginUserName).toHaveBeenCalledWith(expect.any(String));
-      expect(setIsLoggedIn).toHaveBeenCalledWith(true);
+    describe('test newUser onClick functionality', () => {
+      // render form (in beforeEach)
+      it('new user form data is correctly captured and sent to POST onClick', () => {
+        // simulate change for userName
+        fireEvent.change(getById(wrapper.container, 'userName'), { target: { value: 'Lion' } });
+        // simulate change for pass
+        fireEvent.change(getById(wrapper.container, 'password'), { target: { value: 'Lion' } });
+        // wait for userName and pass to update
+        // simulate click
+        fireEvent.click(getById(wrapper.container, 'newUser'));
+        // check if click triggers post request with proper data
+        expect(axios.post).toHaveBeenCalledWith('/api/addUser', { userName: 'Lion', password: 'Lion' });
+      });
+      it('Updates LoginUserName and IsLoggedIn correctly upon sucessful login', async () => {
+        // simulate change for userName
+        fireEvent.change(getById(wrapper.container, 'userName'), { target: { value: 'Lion' } });
+        // simulate click
+        fireEvent.click(getById(wrapper.container, 'newUser'));
+        // simulate successful login
+        await postPromise;
+        // setLoginUserName shout update userName with recieved string
+        expect(setLoginUserName, 'setLoginUserName should have gotten a string but it did not').toHaveBeenCalledWith('Lion');
+        // setIsLoggedIn should update to true
+        expect(setIsLoggedIn, 'setIsLoggedIn should be true but it is not').toHaveBeenCalledWith(true);
+      });
     });
-
-    // it('existing user can login', async () => {
-    //   const knownUser = wrapper.find('#knownUser').at(0);
-    //   knownUser.simulate('click');
-    //   expect(axios.post).toHaveBeenCalledWith('/api/login', { userName: expect.any(String), password: expect.any(String) });
-    //   await postPromise;
-    //   expect(setLoginUserName).toHaveBeenCalledWith(expect.any(String));
-    //   expect(setIsLoggedIn).toHaveBeenCalledWith(true);
-
-    //   const userName = wrapper.find('#userName').at(0);
-    //   const password = wrapper.find('#password').at(0);
-
-    //   userName.value = 'TestUser';
-    //   userName.simulate('change', { target: userName });
-    //   expect(setState).toHaveBeenCalledWith('TestUser');
-
-    //   password.value = 'TestPass';
-    //   password.simulate('change', { target: password });
-    //   expect(setState).toHaveBeenCalledWith('TestPass');
-    // });
   });
-  // describe('friends', () => {
-
-  // });
 });
-
-// describe("Title input", () => {
-//   it("Should capture title correctly onChange", () => {
-//       const title = wrapper.find("input").at(0);
-//       title.instance().value = "Test";
-//       title.simulate("change");
-//       expect(setState).toHaveBeenCalledWith("Test");
-//   });
-// });
